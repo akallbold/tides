19a91bd7d7945fa972242b6f535095b6f4daea21
akallbold <a.kallenbornbolden@gmail.com>
2024-07-29 20:49:31 -0400

    Initial Commit

---
+++ tides/tidesApp.swift
@@ -0,0 +1,17 @@
+//
+//  tidesApp.swift
+//  tides
+//
+//  Created by Anna Kallenborn-Bolden on 7/29/24.
+//
+
+import SwiftUI
+
+@main
+struct tidesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
